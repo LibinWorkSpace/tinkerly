5e6450e615108194a505a501098037ee833e7c80
LibinWorkSpace <libinmani.2.0@gmail.com>
2025-08-05 18:39:59 +0530

    changes

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -8,14 +8,14 @@ class ApiConstants {
       return 'http://localhost:5000';
     } else if (Platform.isAndroid) {
       // For Android emulator during development
-      // return 'http://10.0.2.2:5000';
+      return 'http://10.0.2.2:5000';
 
       // For real Android device connecting to your computer
       // Replace with your computer's IP address
-      return 'http://192.168.1.4:5000';
+      // return 'http://192.168.1.4:5000';
     } else {
       // For iOS and other platforms
-      return 'http://192.168.1.4:5000';
+      return 'http://localhost:5000';
     }
   }
 }
